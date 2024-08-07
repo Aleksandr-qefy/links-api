e826d544deb263e2a44884eb680c47609d8c5176
Qefy <siralexpeter@mail.ru>
2024-08-07 13:05:09 +0300

    add postgres db initializing and configs

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,39 @@ import (
 	"github.com/Aleksandr-qefy/links-api/internal/handler"
 	"github.com/Aleksandr-qefy/links-api/internal/repository"
 	"github.com/Aleksandr-qefy/links-api/internal/service"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
 	if err := InitConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	srvr := &api.Server{}
-	if err := srvr.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+	srv := &api.Server{}
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
